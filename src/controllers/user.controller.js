3fba409df7fb585be585fbe06632a8c29da88675
Lakshay Aggarwal <aggarwallakshay370@gmail.com>
2025-07-16 00:01:11 +0530

    Added change password functionality in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -250,6 +250,28 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPasssword} = req.body
+
+    const user = await User.findById(req.user?.id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Changed Successfully"))
+
+})
+
+
+
+
 export {
     registerUser, 
     loginUser, 
