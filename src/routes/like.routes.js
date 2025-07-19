cdff4df64a3b0d0ec15205e90ff48d476a56ab7d
Lakshay Aggarwal <aggarwallakshay370@gmail.com>
2025-07-19 19:27:13 +0530

    Added Like routes

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 import {
-    getSubscribedChannels,
-    getUserChannelSubscribers,
-    toggleSubscription,
-} from "../controllers/subscription.controller.js"
+    getLikedVideos,
+    toggleCommentLike,
+    toggleVideoLike,
+    toggleTweetLike,
+} from "../controllers/like.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
-
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
 
 export default router
