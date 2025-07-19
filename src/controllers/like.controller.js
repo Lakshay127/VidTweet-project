6c60f797960605226d124c2d243302c441bb5fb1
Lakshay Aggarwal <aggarwallakshay370@gmail.com>
2025-07-19 19:27:42 +0530

    Added Like Controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,12 +7,50 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+    const videoLike = await Like.findOne({video: videoId, user: req.user._id})
+    if (videoLike) {
+        // If like exists, remove it
+        await Like.deleteOne({video: videoId, user: req.user._id})
+        return res.status(200).json({message: "Video like removed"})
+    } 
+    else {
+        // If like does not exist, create it
+        const newLike = new Like({
+            video: videoId,
+            user: req.user._id
+        })
+        await newLike.save()
+        return res.status(201).json({message: "Video liked successfully"})
+    }
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
 
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID")
+    }
+    const commentLike = await Comment.findOne({comment: commentId, video: videoId, user: req.user._id})
+    if (commentLike) {
+        // If like exists, remove it
+        await Like.deleteOne({comment: commentId, video: videoId, user: req.user._id})
+        return res.status(200).json({message: "Comment like removed"})
+    } 
+    else {
+        // If like does not exist, create it
+        const newLike = new Like({
+            comment: commentId,
+            video: videoId,
+            user: req.user._id
+        })
+        await newLike.save()
+        return res.status(201).json({message: "Comment liked successfully"})
+    }
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
