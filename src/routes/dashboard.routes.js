e382039b91da78a33fcfb4cb1a4a8e2170c26fe3
Lakshay Aggarwal <aggarwallakshay370@gmail.com>
2025-07-20 05:30:47 +0530

    Some small correction in routes

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,18 +1,15 @@
 import { Router } from 'express';
 import {
-    getLikedVideos,
-    toggleCommentLike,
-    toggleVideoLike,
-    toggleTweetLike,
-} from "../controllers/like.controller.js"
+    getChannelStats,
+    getChannelVideos,
+} from "../controllers/dashboard.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
+
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
-router.route("/toggle/c/:commentId").post(toggleCommentLike);
-router.route("/toggle/t/:tweetId").post(toggleTweetLike);
-router.route("/videos").get(getLikedVideos);
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
 
 export default router
